ee8ed5a99559393d558b7ee788c6f0fd2f049325
Rachmaninoff <rachmaninoffn2@L02028559.niaid.nih.gov>
2022-09-20 16:45:58 -0500

    update worfklow.sh comments

--- NR_code/workflow.sh
+++ NR_code/workflow.sh
@@ -3,24 +3,24 @@
 #workflow/NOT_RUN_1_read_in_data2_wsp_files.R
 #workflow/NOT_RUN_2_save_expression_mat_and_cellmeta.R
 
-#download the data and convert to rds
+## download the data and convert to rds ----------------------
 
-#Rscript workflow/0_prep_zenodo_data.R
+Rscript workflow/0_prep_zenodo_data.R
 
-#These will generate the outputs necessary for the figures
+# These will generate the outputs necessary for the figures --
 
-##Rscript workflow/3_run_flowsom.R
+Rscript workflow/3_run_flowsom.R
 
-#Rscript workflow/4_check_gates_and_save_rbd_s1_dat.R
-#
-#Rscript workflow/5_save_spike_rbd_summ.R
+Rscript workflow/4_check_gates_and_save_rbd_s1_dat.R
 
-#Rscript workflow/6_umap_subsampleB.R
-#
-#Rscript workflow/7_anova_models.R
-#
-##making the figures
-#
+Rscript workflow/5_save_spike_rbd_summ.R
+
+Rscript workflow/6_umap_subsampleB.R
+
+Rscript workflow/7_anova_models.R
+
+# making the figures ----------------------------------------
+
 Rscript workflow/paper_figures/antibody_plots/ab_cor_plots.R
 Rscript workflow/paper_figures/antibody_plots/ab_d2d28_density.R
 Rscript workflow/paper_figures/cluster_timecourse_plots.R
